20547945d836f5a38bd556998fcbeabd6076b096
MariskaEsaPurnomo <mariskaesapurnomo@gmail.com>
2024-02-27 11:18:37 +0700

    Add project files.

---
+++ PertemuanPertamaKelasD/PertemuanPertamaKelasD.cpp
@@ -0,0 +1,20 @@
+// PertemuanPertamaKelasD.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{//begin
+
+	int nAlas, nTinggi;
+	double nLuas;
+	cout << "Masukkan Alasnya = ";
+	cin >> nAlas;
+	cout << "Masukan Tingginya = ";
+	cin >> nTinggi;
+	nLuas = 0, 5 * (nAlas * nTinggi);
+	cout << "Luasnya adalah = " << nLuas << endl;
+
+	system("pause");
+
+}
